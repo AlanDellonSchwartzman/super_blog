f8dd5c0143507dd5f47af2f5ea1872c770a115a8
Alan Dellon Schwartzman <alandellon0@gmail.com>
2019-08-26 23:57:40 -0300

    Entity alter and form initial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     
-    @post.user_id = current_user
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
